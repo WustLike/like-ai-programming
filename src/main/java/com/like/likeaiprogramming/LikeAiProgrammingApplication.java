76676bac820778171e8e6f029c6a284db9eb9adf
李柯19436 <ke.li@going-link.com>
2026-06-03 17:21:36 +0800

    .init

---
+++ src/main/java/com/like/likeaiprogramming/LikeAiProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.like.likeaiprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeAiProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeAiProgrammingApplication.class, args);
+    }
+
+}
