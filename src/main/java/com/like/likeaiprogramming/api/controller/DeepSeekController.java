7ce52216f18652dd37623de84b7e948dd70f2c78
李柯19436 <ke.li@going-link.com>
2026-06-04 14:48:28 +0800

    [ADD] 简单对话

--- src/main/java/com/like/likeaiprogramming/api/controller/DeepSeekController.java
+++ src/main/java/com/like/likeaiprogramming/api/controller/DeepSeekController.java
@@ -3,7 +3,6 @@ package com.like.likeaiprogramming.api.controller;
 import com.like.likeaiprogramming.api.dto.ChatRequest;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
