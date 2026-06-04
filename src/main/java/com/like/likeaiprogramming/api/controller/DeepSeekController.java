6986a39216e93e230ffceaaf8782f35eb28e65a7
李柯19436 <ke.li@going-link.com>
2026-06-04 16:16:59 +0800

    [ADD] 新增对话页面

--- src/main/java/com/like/likeaiprogramming/api/controller/DeepSeekController.java
+++ src/main/java/com/like/likeaiprogramming/api/controller/DeepSeekController.java
@@ -1,27 +1,35 @@
 package com.like.likeaiprogramming.api.controller;
 
 import com.like.likeaiprogramming.api.dto.ChatRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/deepseek")
+@RequestMapping("/api/ai")
 public class DeepSeekController {
+    private static final Logger logger = LoggerFactory.getLogger(DeepSeekController.class);
 
     @Autowired
     private ChatClient chatClient;
 
     // 简单对话接口
-    @PostMapping("/simple")
-    public String chat(@RequestBody ChatRequest request) {
-        return chatClient.prompt()
-                .user(request.getMessage()) // 用户输入
-                .call()                     // 调用AI
-                .content();                 // 获取文本回复
+    @PostMapping("/chat")
+    public Map<String, Object> chat(@RequestBody ChatRequest request) {
+        logger.info("========================Chat Beginning========================");
+        String responseStr =  chatClient.prompt().user(request.getMessage()).call().content();
+        Map<String, Object> response = new HashMap<>();
+        response.put("ok", true);
+        response.put("success", true);
+        response.put("content", responseStr);
+        return response;
     }
 
 }
